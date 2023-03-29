e64f243fbe4c8511168792918b78f9bdea82a8f8
FrederoxDev <blakefreddie96@gmail.com>
2023-03-29 21:11:01 +0100

    Start com.mojang integration

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,47 @@
-import { compileAddon, Target } from "./lib.ts";
+import { CompilationTarget, compileAddon, CompilerConfig } from "./lib.ts";
+import { parse } from "https://deno.land/std@0.181.0/flags/mod.ts";
 
-compileAddon({
-    packName: "packName",
+const version = "1.0.0";
+
+// Get CLI Flags
+const flags = parse(Deno.args, {
+    boolean: ["version", "v", "preview"],
+    alias: {
+        v: "version"
+    }
+});
+
+// Exit if the flag for version was used
+if (flags.version) {
+    console.log(`Version ${version}`);
+    Deno.exit(0);
+}
+
+// If there were no flags passed
+if (flags._[0] === undefined) {
+    console.log("flags: [-v | --version] [--preview]");
+    console.log("commands: [watch] [package]")
+    Deno.exit(1);
+}
+
+const packConfig: CompilerConfig = {
+    packName: "ComputerCraft",
     behaviourPackPath: "/BP/",
     resourcePackPath: "/RP/"
-});
+}
+
+if (flags._[0] === "watch") {
+    await compileAddon(packConfig, flags.preview ? CompilationTarget.Preview : CompilationTarget.Stable);
+
+    console.log("Starting watch mode...")
+    // Todo: Watch both RP And BP
+    let watcher = Deno.watchFs(Deno.cwd() + packConfig.behaviourPackPath);
+
+    for await (const event of watcher) {
+        console.log(">>>> event", event);
+    }
+}
+
+if (flags._[0] === "package") {
+    await compileAddon(packConfig, CompilationTarget.Packaged);
+}
--- src/lib.ts
+++ src/lib.ts
@@ -1,16 +1,27 @@
-import { copySync, emptyDirSync, copy } from "https://deno.land/std@0.181.0/fs/mod.ts";
+import { emptyDirSync, copy } from "https://deno.land/std@0.181.0/fs/mod.ts";
 import { transform } from "https://deno.land/x/swc@0.2.1/mod.ts";
+
+const localAppData = Deno.env.get('localappdata');
+const stablePackage = "Microsoft.MinecraftUWP_8wekyb3d8bbwe";
+const previewPackage = "Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe";
+const comMojang = "LocalState\\games\\com.mojang"
+
 
 /**
  * Stores the intended compilation target
  */
-export enum Target {
+export enum CompilationTarget {
     /**Export directly as a .mcaddon File*/
     Packaged,
     /**Export to the stable version of minecraft */
     Stable,
     /**Export to the preview version of minecraft */
     Preview
+}
+
+export enum PackType {
+    Behaviour,
+    Resource
 }
 
 /**
@@ -22,42 +33,56 @@ export interface CompilerConfig {
     resourcePackPath?: string
 }
 
-/**
- * Compiles the entire pack 
- */
-export async function compileAddon(config: CompilerConfig): Promise<void> {
+export async function compileAddon(config: CompilerConfig, target: CompilationTarget): Promise<void> {
     let count = 0;
     const start = performance.now();
+    console.log("Starting addon compilation...")
 
     if (config.behaviourPackPath !== undefined) {
-        count += await compileDirectory(config.behaviourPackPath);
+        count += await compileDirectory("", target, PackType.Behaviour, config.packName);
     } 
 
     if (config.resourcePackPath !== undefined) {
-        count += await compileDirectory(config.resourcePackPath);
+        count += await compileDirectory("", target, PackType.Resource, config.packName);
     } 
 
     const timeTaken = performance.now() - start;
     console.log(`Compiled ${count} files in ${timeTaken}ms`)
 }
 
-/**
- * Compiles a specific directory
- */
-async function compileDirectory(path: string): Promise<number> {
+function targetToPath(target: CompilationTarget, packType: PackType, packName: string): string {
+    const packPath = packType === PackType.Behaviour ? "BP" : "RP";
+    const folderName = packType === PackType.Behaviour ? "development_behavior_packs" : "development_resource_packs";
+
+    if (target === CompilationTarget.Packaged) {
+        return `${Deno.cwd()}/dist/${packName} ${packPath}/`
+    }
+
+    else if (target === CompilationTarget.Stable) {
+        return `${localAppData}/Packages/${stablePackage}/${comMojang}/${folderName}/${packName} ${packPath}/`
+    }
+
+    else if (target === CompilationTarget.Preview) {
+        return `${localAppData}/Packages/${previewPackage}/${comMojang}/${folderName}/${packName} ${packPath}/`
+    }
+
+    throw new Error(`Unknown Pack Type ${packType}`);
+}
+
+async function compileDirectory(path: string, target: CompilationTarget, packType: PackType, packName: string): Promise<number> {
     let fileCount = 0;
-    const packDir = Deno.cwd() + path;
-    const destDir = Deno.cwd() + "/Packaged/" + path;
+    const packDir = Deno.cwd() + (packType === PackType.Behaviour ? "/BP/" : "/RP/") + path;
+    const destDir = targetToPath(target, packType, packName) + path;
     emptyDirSync(destDir);
 
     // Go through each file & Directory
-    for await (const dirEntry of Deno.readDirSync(Deno.cwd() + path)) {
+    for await (const dirEntry of Deno.readDirSync(packDir)) {
         if (dirEntry.isFile) {
             fileCount += 1;
             compileFile(packDir, destDir, dirEntry.name);
         }
         else if (dirEntry.isDirectory) {
-            fileCount += await compileDirectory(path + `${dirEntry.name}/`)
+            fileCount += await compileDirectory(path + `${dirEntry.name}/`, target, packType, packName)
         }
     }
 
@@ -75,9 +100,9 @@ async function compileFile(packPath: string, destPath: string, fileName: string)
                 },
             },
         }).code;
-        await Deno.writeTextFile(destPath + `/${fileName.replace(".ts", ".js")}`, transpiledJs)
+        await Deno.writeTextFile(destPath + `${fileName.replace(".ts", ".js")}`, transpiledJs)
     }
     else {
-        await copy(`${packPath}/${fileName}`, `${destPath}/${fileName}`, { overwrite: true })
+        await copy(`${packPath}/${fileName}`, `${destPath}${fileName}`, { overwrite: true })
     }
 }
